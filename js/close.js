c431427a9c901eaff5877f95361ba0a4c0843723
WatchMoons <watchmoon120@gmail.com>
2021-09-27 19:12:48 +0700

    v0.2
    wwewqeq

--- js/close.js
+++
@@ -1,8 +0,0 @@
-function scroll() {
-  var x = document.getElementById("scroll");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
